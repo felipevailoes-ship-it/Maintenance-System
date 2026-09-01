eb4437c0ac3e180fae97bb9469e3618f0326467f
felipe_vailoes <Thrashmetal@1998>
2026-08-31 21:57:36 -0300

    estudos com js

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -6,6 +6,7 @@ const systemName ="Sistema de controle de manutenção";
 let activeEquipaments = 48;
 let maintenanceEquipments = 5;
 let preventiveMaintenance = 10;
+let inMaintenance = 5;
 
 console.log("Nome do sistema: " +systemName);
 console.log("Quantidade de equipamentos ativos " +activeEquipaments);
@@ -31,7 +32,56 @@ const equipments = [
 
 console.table(equipments);
 
-
 const activesTotal = document.querySelector("#activesTotal");
-console.log("activeTotal: "+ activesTotal.textContent);
-activesTotal.textContent = 50; 
+const preventiveTotal = document.querySelector("#preventiveTotal");
+const maintenanceEquipmentsTotal = document.querySelector("#maintenanceEquipmentsTotal");
+const equipmentsTable = document.querySelector("#equipmentsTable");
+const searchInput = document.getElementById("searchInput");
+const btnNewEquipment = document.getElementById("btnNewEquipment");
+
+console.log("activeTotal: " + activesTotal.textContent);
+
+function dashboardRefresh() {
+
+    const actives = equipments.filter(
+        equipment => equipment.status === "true"
+    ).length;
+
+    const inMaintenance = equipments.filter(
+        equipment => equipment.status === "maintenance"
+    ).length;
+
+    activesTotal.textContent = actives;
+    maintenanceEquipamentsTotal.textContent = inMaintenance;
+
+    console.log("Dashboard atualizado");
+}
+
+dashboardRefresh();
+
+//function equipmentsTableRender(list) {
+    //equipmentsTable.innerHTML = "";
+
+    //list.forEach(equipment => {
+        //const row = document.createElement("tr");
+
+       // row.innerHTML = `<td>${equipment.name}</td>`
+
+       // equipmentsTable.appenChild(row);
+    //});
+//};
+
+//equipmentsTableRender(equipments);
+
+//searchInput.addEventListener("input", function () {
+    //const term = searchInput.value.toLowerCase();
+
+    //const result = equipments.filter(equipment =>
+        //equipment.name.toLowerCase().includes(term));
+
+        //equipmentsTableRender(result);
+//});
+
+btnNewEquipment.addEventListener("click", function () {
+    console.log("Tá clicante");
+});
